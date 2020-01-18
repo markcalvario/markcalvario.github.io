faf8e2fcca9235cd1efd9651557da7d04253d3ae
markcalvario <58496944+markcalvario@users.noreply.github.com>
2020-01-18 11:27:53 -0500

    fasdo 11:27 am

--- js/index.js
+++ js/index.js
@@ -2,13 +2,13 @@ document.addEventListener("DOMContentLoaded", function(event) {
 	var navbar= document.getElementById("navbar")
 	var sticky= navbar.offsetTop;
 
-	function myFunction() {
-		if (window.pageYOffset >= sticky) {
-			navbar.classList.add("sticky")
-		} else{
-			navbar.classList.remove("sticky")
-		}
+function myFunction() {
+	if (window.pageYOffset >= sticky) {
+		navbar.classList.add("sticky")
+	} else{
+		navbar.classList.remove("sticky")
 	}
-	window.onscroll= function () {myFunction()};
+}
+window.onscroll= function () {myFunction()};
 });
 
