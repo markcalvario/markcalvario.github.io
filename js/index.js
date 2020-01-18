01a553c64952eb3f972e69ec9b50fd6b123d36e2
markcalvario <58496944+markcalvario@users.noreply.github.com>
2020-01-17 22:39:40 -0500

    made the nav bar

---
+++ js/index.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function(event) { 
+	var navbar= document.getElementById("navbar")
+	var sticky= navbar.offsetTop;
+
+	function myFunction() {
+		if (window.pageYOffset >= sticky) {
+			navbar.classList.add("sticky")
+		} else{
+			navbar.classList.remove("sticky")
+		}
+	}
+	window.onscroll= function () {myFunction()};
+});
+
