2ad2e890a6caafcdf4a3a68f983fb12c913cebc7
markcalvario <58496944+markcalvario@users.noreply.github.com>
2020-05-15 08:58:07 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,14 +1,23 @@
+/** Sticky NavBar **/
 document.addEventListener("DOMContentLoaded", function(event) { 
-	var navbar= document.getElementById("navbar")
-	var sticky= navbar.offsetTop;
+var navbar= document.getElementById("myTopnav")
+var sticky= navbar.offsetTop;
 
 function myFunction() {
-	if (window.pageYOffset >= sticky) {
-		navbar.classList.add("sticky")
-	} else{
-		navbar.classList.remove("sticky")
-	}
+   if (window.pageYOffset >= sticky) {
+       navbar.classList.add("sticky")
+   } else{
+       navbar.classList.remove("sticky")
+   }
 }
 window.onscroll= function () {myFunction()};
 });
-
+/** End of Sticky NavBar **/
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
