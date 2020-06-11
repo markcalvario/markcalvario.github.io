2a32b8bf1f08fcc9a6e42d9e0bc34546a7196944
Mark Calvario <58496944+markcalvario@users.noreply.github.com>
2020-06-11 01:35:31 -0700

    added 2nd row of ceramic images to ceramic pg

--- js/about.js
+++ js/about.js
@@ -2,6 +2,7 @@
 var boolean= true;
 function moveBallDown(){
 	var ball= document.getElementById('ball');
+
 	if (boolean){
 		ball.style.marginTop= "200px";
 		boolean= false;
@@ -10,3 +11,27 @@ function moveBallDown(){
 		boolean= true;
 	}	
 }
+
+ /** Sticky NavBar **/
+       document.addEventListener("DOMContentLoaded", function(event) { 
+       var navbar= document.getElementById("myTopnav")
+       var sticky= navbar.offsetTop;
+
+       function myFunction() {
+           if (window.pageYOffset >= sticky) {
+               navbar.classList.add("sticky")
+           } else{
+               navbar.classList.remove("sticky")
+           }
+       }
+       window.onscroll= function () {myFunction()};
+       });
+       /** End of Sticky NavBar **/
+       function myFunction() {
+          var x = document.getElementById("myTopnav");
+          if (x.className === "topnav") {
+            x.className += " responsive";
+          } else {
+            x.className = "topnav";
+          }
+        }
